4a7ed6d7dc9f063dd14e139b3fee7aa4b16faee9
TRIF3X <thewizardofcrypto@gmail.com>
2019-01-04 10:54:29 -0600

    styling components with style jsx

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a4c767d1c5253b1cb85f";
+/******/ 	var hotCurrentHash = "dee589dcd338ccfafbe8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
